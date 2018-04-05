8a12d4c8893b820a0ec0b8ed1c6dcbb91cc3a8e7
Tristan RAMIREZ <tristan.rami@gmail.com>
2018-04-05 17:35:20 +0200

    login + register

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+/* GET sign in page. */
+router.get('/login', function(req, res, next) {
+  res.render('signin');
+});
+/* GET sign up page. */
+router.get('/register', function(req, res, next) {
+  res.render('signup');
+});
 
 module.exports = router;
