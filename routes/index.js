3895e3022a3b3e6e4478f5a711fbfec4f4a1445d
Tristan RAMIREZ <tristan.rami@gmail.com>
2018-04-13 11:53:05 +0200

    page stats

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		//req.flash('error_msg','You are not logged in');
+		var isAuthenticated = req.isAuthenticated();
+		res.render('home', {authenticated: isAuthenticated});
+	}
+}
+
 // Get Homepage
 router.get('/', ensureAuthenticated, function(req, res){
 	var isAuthenticated = req.isAuthenticated();
@@ -17,14 +27,10 @@ router.get('/account', ensureAuthenticated, function(req, res){
 	res.render('account', {authenticated: isAuthenticated});
 });
 
-function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	} else {
-		//req.flash('error_msg','You are not logged in');
-		var isAuthenticated = req.isAuthenticated();
-		res.render('home', {authenticated: isAuthenticated});
-	}
-}
+// Get Stats page
+router.get('/stats', ensureAuthenticated, function(req, res){
+	var isAuthenticated = req.isAuthenticated();
+	res.render('stats', {authenticated: isAuthenticated});
+});
 
 module.exports = router;
